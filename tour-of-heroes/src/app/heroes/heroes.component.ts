b1f7802900a7e69530a2f4e2a1090e5b9684cbd9
Silber <silber.lima@gmail.com>
2023-03-09 16:39:24 -0300

    RxJs and messages

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,21 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../services/hero.service';
 import { Hero } from './hero.model';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent implements OnInit{
+export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name: "Spider Man"
-  };
-
-  constructor(){}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
+    this.heroService.getHeroes().subscribe(
+      (heroes) => this.heroes = heroes,
+      (err) => console.log(err),
+      () => console.log('Ok')
+    );
+  }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 }

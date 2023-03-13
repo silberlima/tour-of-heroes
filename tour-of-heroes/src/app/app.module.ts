2b9bd8e0179e01d623398ddc7032b8aa43672de9
Silber <silber.lima@gmail.com>
2023-03-13 10:58:55 -0300

    Organize

--- tour-of-heroes/src/app/app.module.ts
+++ tour-of-heroes/src/app/app.module.ts
@@ -5,16 +5,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './heroes/hero-detail/hero-detail/hero-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./heroes/hero.model";
+
+export const HEROES : Hero[] = [
+  {id:1, name: "Spider Man"},
+  {id:2, name: "Iron Man"},
+  {id:3, name: "Captain America"},
+  {id:4, name: "Hulk"},
+  {id:5, name: "Thor"},
+
+];
